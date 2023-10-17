c9b6792475843526e0fed2ac0f95b1616cf6fb04
Imam Robani <imamrob@gmail.com>
2023-10-17 21:27:39 +0700

    Initial Commit

---
+++ FoodMarket/FoodMarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodMarketApp.swift
+//  FoodMarket
+//
+//  Created by Imam Robani on 17/10/23.
+//
+
+import SwiftUI
+
+@main
+struct FoodMarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
